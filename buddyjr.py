187dc08dd6cb458127a05552b0281be550801ace
Kevin McAleer <kevinmcaleer@gmail.com>
2024-04-06 18:27:39 +0100

    working

--- buddyjr.py
+++ buddyjr.py
@@ -1,12 +1,33 @@
 import board
 import busio
 import adafruit_pca9685
+from time import sleep
+import adafruit_motor.servo 
+from adafruit_servokit import ServoKit
+
 
 i2c = busio.I2C(board.SCL, board.SDA)
 pca = adafruit_pca9685.PCA9685(i2c)
 
-# Set the PWM frequency to 60hz
-pca.frequency = 60
+# Set the PWM frequency to 50hz
+pca.frequency = 50
 
 # Set the duty cycle for channel 0 to 50%
-pca.channels[0].duty_cycle = 0x7fff
+pca.channels[1].duty_cycle = 150
+pca.channels[2].duty_cycle = 1000
+
+kit = ServoKit(channels=16)
+channel = 1
+
+servo = adafruit_motor.servo.Servo(pca)
+servo.actuation_range = 135
+
+# servo = adafruit_motor.servo.Servo(channel)
+
+kit.servo[channel].angle = 45
+
+kit.servo[2].angle = 90  
+sleep(1)
+
+kit.servo[channel].angle = 90
+
---
+++ demo1.py
@@ -0,0 +1,38 @@
+import board
+import busio
+from adafruit_servokit import ServoKit
+import adafruit_pca9685
+from time import sleep
+
+# Specify the number of channels on the PCA9685 module
+kit = ServoKit(channels=16)
+
+# Initialize I2C bus
+i2c = busio.I2C(scl=board.SCL, sda=board.SDA)
+
+pca = adafruit_pca9685.PCA9685(i2c)
+pca.frequency = 50
+pca.channels[0].duty_cycle = 1500
+pca.channels[1].duty_cycle = 500
+
+# Define servos on specific channels
+servo1 = kit.servo[0]
+servo2 = kit.servo[1]
+servo3 = kit.servo[2]
+servo4 = kit.servo[3]
+
+print("Moving the servos")
+# Example to set servo to 180 degrees
+servo1.angle = 90
+servo2.angle = 90
+servo3.angle = 90
+servo4.angle = 90
+
+sleep(1)
+
+servo1.angle = 0
+servo2.angle = 0
+servo3.angle = 0
+servo4.angle = 0
+# Adjust as necessary for your specific servos and desired positions
+print("done.")
